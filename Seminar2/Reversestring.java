042dd5128867c50abb58a4bfb8d100340d651de9
RSLuca <for.school.studying.5@gmail.com>
2025-04-06 14:28:34 +0000

    add: fizzbuzz & pattern

---
+++ Seminar2/Reversestring.java
@@ -0,0 +1,24 @@
+package Seminar2;
+
+public class Reversestring {
+    public static void reverse(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while(left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+
+            left++;
+            right--;
+        }
+
+        String string = String.valueOf(s);
+        System.out.println(string);
+    }
+    public static void main(String[] args) {
+        String s = "Ana are mere";
+        reverse(s.toCharArray());
+    }
+}
